aa01ea4d550070b795024b01a5d242fdd46789d7
anurag41682 <anuragyadav41682@gmail.com>
2024-01-28 02:41:03 +0530

    Hide Menu Bar

--- electron/main.ts
+++ electron/main.ts
@@ -24,6 +24,7 @@ function createWindow() {
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
+    autoHideMenuBar: true,
   })
 
   // Test active push message to Renderer-process.
