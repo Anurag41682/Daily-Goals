04e4723d9ece305d22ed3e6cc6e60929751c2188
anurag41682 <anuragyadav41682@gmail.com>
2024-01-29 02:52:18 +0530

    Added task Area & AddButton

--- src/components/landingPage.tsx
+++ src/components/landingPage.tsx
@@ -1,5 +1,8 @@
 import { ModeToggle } from "./mode-toggle";
 import { useState, useEffect } from "react";
+import Tasks from "./tasks";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 const LandingPage = () => {
   const [currentTime, setCurrentTime] = useState(new Date());
   useEffect(() => {
@@ -18,13 +21,20 @@ const LandingPage = () => {
     "Saturday",
   ];
   return (
-    <div className="landing-page">
+    <div className=" items-center landing-page w-full h-full">
       <div className="flex justify-end pt-1 pr-1">
         <ModeToggle />
       </div>
-      <div className="flex flex-col font-bold text-lg justify-center items-center">
-        <h1>Day: {daysOfWeek[currentTime.getDay()]}</h1>
-        <h1>Current Time: {currentTime.toLocaleTimeString()}</h1>
+      <div className="w-full h-5/6 flex flex-col font-bold text-lg justify-start items-center">
+        <div className="flex flex-col justify-center items-center">
+          <h1>Day: {daysOfWeek[currentTime.getDay()]}</h1>
+          <h1>Current Time: {currentTime.toLocaleTimeString()}</h1>
+        </div>
+        <Tasks />
+        <div className="mt-3 grid w-1/2 gap-2">
+          <Textarea className="h-px" placeholder="Enter your tasks" />
+          <Button>Add</Button>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/tasks.tsx
@@ -0,0 +1,9 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+const Tasks = () => {
+  return (
+    <ScrollArea className="mt-4 h-full w-3/4 rounded-md border p-4">
+      <div>Task</div>
+    </ScrollArea>
+  );
+};
+export default Tasks;
