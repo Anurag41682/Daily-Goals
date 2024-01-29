e749a24d407ea94c8d0fa2317412713b1a79768c
anurag41682 <anuragyadav41682@gmail.com>
2024-01-29 06:02:35 +0530

    Made TaskForm Responsive

--- src/components/tasks.tsx
+++ src/components/tasks.tsx
@@ -1,4 +1,7 @@
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
+
 interface taskProps {
   taskList: string[];
 }
@@ -7,7 +10,12 @@ const Tasks: React.FC<taskProps> = (props) => {
   return (
     <ScrollArea className="mt-4 h-full w-3/4 rounded-md border p-4">
       {props.taskList.map((item, idx) => (
-        <div key={idx}>{item}</div>
+        <div className="space-x-2 flex items-center pl-2 pt-2">
+          <Checkbox className="h-5 md:h-6 lg:h-7 w-5 md:w-6 lg:w-7" />
+          <Label className="text-sm md:text-lg lg:text-xl" key={idx}>
+            {item}
+          </Label>
+        </div>
       ))}
     </ScrollArea>
   );
