7ffe5681cb7d5360d8fba5f369d19b55254e3c05
anurag41682 <anuragyadav41682@gmail.com>
2024-01-31 04:25:40 +0530

    Added Edit Icon

--- src/components/tasks.tsx
+++ src/components/tasks.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
+import { EditIcon } from "lucide-react";
 
 interface taskProps {
   taskList: string[];
@@ -9,27 +10,36 @@ interface taskProps {
 
 const Tasks: React.FC<taskProps> = (props) => {
   const [isLined, setIsLined] = useState<boolean[]>([]);
-  const handleClick = (idx: number) => {
+  const handleEditClick = () => {
+    console.log("Clicked");
+  };
+  const handleCheckboxClick = (idx: number) => {
     isLined[idx] = !isLined[idx];
     setIsLined(isLined);
   };
   return (
     <ScrollArea className="mt-4 h-full w-3/4 rounded-md border p-4">
       {props.taskList.map((item, idx) => (
-        <div key={idx} className="space-x-4 flex items-center pl-2 pt-2">
-          <Checkbox
-            onClick={() => {
-              handleClick(idx);
-            }}
-            className="h-5 md:h-6 lg:h-7 w-5 md:w-6 lg:w-7"
+        <div key={idx} className=" flex justify-between items-center p-2">
+          <div className="space-x-4 flex items-center">
+            <Checkbox
+              onClick={() => {
+                handleCheckboxClick(idx);
+              }}
+              className="h-5 md:h-6 lg:h-7 w-5 md:w-6 lg:w-7"
+            />
+            <Label
+              className={`${
+                isLined[idx] ? "line-through" : ""
+              } text-sm  md:text-lg lg:text-xl`}
+            >
+              {item}
+            </Label>
+          </div>
+          <EditIcon
+            onClick={handleEditClick}
+            className="w-5 md:w-6 lg:w-7 h-5 md:h-6 lg:h-7"
           />
-          <Label
-            className={`${
-              isLined[idx] ? "line-through" : ""
-            } text-sm  md:text-lg lg:text-xl`}
-          >
-            {item}
-          </Label>
         </div>
       ))}
     </ScrollArea>
